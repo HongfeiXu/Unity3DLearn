6c969327a1b67746cc50ef8ca554ab29c5026bc3
HongfeiXu <icevmj@gmail.com>
2021-02-01 15:31:29 +0800

    AssetBundle dependency

--- AssetBundle/Assets/Scripts/AssetBundleLoadWWW.cs
+++ AssetBundle/Assets/Scripts/AssetBundleLoadWWW.cs
@@ -1,22 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class AssetBundleLoadWWW : MonoBehaviour
 {
-	private string localPath = "/ABs/unlit_0_ab";
+	private string localPath = "/ABs/mat_ab";
+	private string manifestFileLocalPath = "/ABs/ABs";
 
-	// Start is called before the first frame update
-	void Start()
-    {
-		//StartCoroutine(GetABFromFile());
-		StartCoroutine(GetABFromWWW());
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(10, 10, 150, 50), "GetABFromFile"))
+		{
+			StartCoroutine(GetABFromFile());
+		}
+		if (GUI.Button(new Rect(10, 70, 150, 50), "GetABFromWWW"))
+		{
+			StartCoroutine(GetABFromWWW());
+		}
 	}
 
 	IEnumerator GetABFromFile()
 	{
-        string file_prefab_bundle = "file://" + Application.dataPath + localPath;
-        WWW data = new WWW(file_prefab_bundle);
+		// 加载依赖ab包
+		AssetBundle assetBundle = AssetBundle.LoadFromFile(Application.dataPath + manifestFileLocalPath);
+		AssetBundleManifest manifest = assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+		string[] dependencies = manifest.GetAllDependencies("mat_ab");
+		foreach (string dependency in dependencies)
+		{
+			string dependencyFilePath = Path.Combine(Application.dataPath + "/ABs", dependency);
+			Debug.Log("dependencyFilePath = " + dependencyFilePath);
+			AssetBundle.LoadFromFile(dependencyFilePath);
+		}
+		// 加载目标ab包
+		string file_prefab_bundle = "file://" + Application.dataPath + localPath;
+		WWW data = new WWW(file_prefab_bundle);
         yield return data;
 		Material mat = (Material)data.assetBundle.LoadAsset("Unlit_0");
 		GetComponent<MeshRenderer>().material = mat;
@@ -24,6 +42,9 @@ public class AssetBundleLoadWWW : MonoBehaviour
 
 	IEnumerator GetABFromWWW()
 	{
+		// TODO: 加载依赖ab包 
+
+		// 加载目标ab包
 		string url_prefab_bundle = "http://127.0.0.1:8000" + localPath;
 		WWW data = new WWW(url_prefab_bundle);
 		yield return data;
