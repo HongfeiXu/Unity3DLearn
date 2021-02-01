6203327b5a06ac7d6f476c29b0edf42f4d5b29d2
HongfeiXu <icevmj@gmail.com>
2021-02-01 23:13:17 +0800

    ABManager.cs and bugfix

---
+++ AssetBundle/Assets/Scripts/ABLoadManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+/// <summary>
+/// 
+/// </summary>
+public class ABLoadManager
+{
+	/// <summary>
+	/// 单例
+	/// </summary>
+	private static ABLoadManager m_Instance;
+	public static ABLoadManager Instance
+	{
+		get
+		{
+			if (null == m_Instance)
+            {
+                m_Instance = new ABLoadManager();
+                m_Instance.Init();
+            }
+            return m_Instance;
+		}
+	}
+
+
+	private string m_abFolder = "";
+	private Dictionary<string, AssetBundle> m_abDic = new Dictionary<string, AssetBundle>();
+
+	private AssetBundleManifest m_manifest;
+
+	/// <summary>
+	/// 初始化一些路径，以及ab包的manifest
+	/// </summary>
+	void Init()
+	{
+		m_abFolder = Path.Combine(Application.dataPath, "ABs");
+		AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(m_abFolder, "ABs"));
+		m_manifest = assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+	}
+
+	public AssetBundle LoadAssetBundle(string abName)
+	{
+		// 加载目标ab包
+		AssetBundle target_ab = null;
+		if (!m_abDic.ContainsKey(abName))
+		{
+			string abPath = Path.Combine(m_abFolder, abName);
+            target_ab = AssetBundle.LoadFromFile(abPath);
+			m_abDic[abName] = target_ab;
+		}
+		else
+		{
+            target_ab = m_abDic[abName];
+		}
+
+		// 加载依赖
+		string[] dependencies = m_manifest.GetAllDependencies("mat_ab");
+        AssetBundle ab = null;
+        foreach (string dependency in dependencies)
+		{
+			Debug.Log("dependency = " + dependency);
+			if (!m_abDic.ContainsKey(dependency))
+			{
+				string abPath = Path.Combine(m_abFolder, dependency);
+				Debug.Log("abPath = " + abPath);
+				ab = AssetBundle.LoadFromFile(abPath);
+				m_abDic[dependency] = ab;
+			}
+		}
+
+		return target_ab;
+	}
+}
---
+++ AssetBundle/Assets/Scripts/ABLoaderTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// AssetBundle加载示例：加载ab包中的材质，替换到当前gameobject上
+/// </summary>
+public class ABLoaderTest : MonoBehaviour
+{
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 50), "GetABFromWWW"))
+        {
+            StartCoroutine(GetABFromWWW());
+        }
+        if (GUI.Button(new Rect(10, 70, 150, 50), "Test1"))
+        {
+            AssetBundleTest1();
+        }
+    }
+
+    void AssetBundleTest1()
+    {
+        // 加载目标ab包
+        AssetBundle matAB = ABLoadManager.Instance.LoadAssetBundle("mat_ab");
+        if (matAB == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        // 设置材质
+        Material mat = (Material)matAB.LoadAsset("Unlit_0");
+        GetComponent<MeshRenderer>().material = mat;
+    }
+
+    /// <summary>
+    /// 从服务器下载
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator GetABFromWWW()
+    {
+        // TODO: 加载依赖ab包 
+
+
+        // 加载目标ab包
+        string url_mat_bundle = "http://127.0.0.1:8000" + "/ABs/mat_ab";
+        WWW data = new WWW(url_mat_bundle);
+        yield return data;
+        Material mat = (Material)data.assetBundle.LoadAsset("Red");
+        GetComponent<MeshRenderer>().material = mat;
+    }
+
+}
